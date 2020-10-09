1c307504b9d688aa4f4b63571fad92b856e0ba34
Alex M <alexmoga147@gmail.com>
2020-10-09 15:07:06 +0200

    Alex 1 Part casi funcionando

---
+++ pollutantmean.R
@@ -0,0 +1,53 @@
+# R Programming: Programmimg Assignament
+# Part 1
+# By Alex M
+
+#Write a function named 'pollutantmean' that calculates the mean of a pollutant 
+#(sulfate or nitrate) across a specified list of monitors. The function 'pollutantmean' 
+#takes three arguments: 'directory', 'pollutant', and 'id'. Given a vector monitor ID 
+#numbers, 'pollutantmean' reads that monitors' particulate matter data from the 
+#directory specified in the 'directory' argument and returns the mean of the 
+#pollutant across all of the monitors, ignoring any missing values coded as NA. 
+#A prototype of the function is as follows
+
+print(R.version.string)
+
+
+pollutantmean<-function(directory,pollutant, id=1:332) {
+  
+  print(directory)
+  
+  # leemos los archivos
+  
+  cont<-0
+  for (i in id) {
+          #preparamos el nombre del fichero
+          if (i<10) { filename<-paste("00",i,sep="")}
+          if (i>=10) { filename<-paste("0",i,sep="")}
+          if (i>=100) { filename<-paste(i,sep="")}
+    
+          filename<-paste(filename,".csv",sep="")
+          print(filename)      
+          filepath<-paste(getwd(),"/",directory,"/",filename,sep="")
+          print(filepath)      
+          #guardamos el contenido del fichero
+          dataaux<-read.csv(filepath, header=TRUE, sep=",")
+          if (cont==0)  {Alldata<-dataaux}
+            else { Alldata<- merge.data.frame(Alldata,dataaux, all=TRUE)
+            }
+          cont<-cont+1
+          str(Alldata)
+     }
+  
+  #Una vez almacenados los ficheros calculamos la media del atriubuto solicitado
+  AlldataNA<-Alldata[!is.na(Alldata$sulfate) & !is.na(Alldata$nitrate), ]
+  #str(AlldataNA)
+  if(identical(pollutant,"nitrate")) M<-mean(AlldataNA$nitrate)
+  if(identical(pollutant,"sulfate")) M<-mean(AlldataNA$sulfate)
+  M
+  
+}
+
+
+pollutantmean("specdata","nitrate",23)
+
